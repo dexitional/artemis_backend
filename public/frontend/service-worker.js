ed5552458d5003e0f9dbf5a7f1ee58761ab36b6f
Ebenezer Kwabena Blay Ackah <ebenezerkb.ackah@gmail.com>
2021-10-14 14:56:55 +0000

    fix

--- public/frontend/service-worker.js
+++ public/frontend/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.460ddf5924d6281db7e4bcb11f527066.js"
+  "/precache-manifest.733f6f6e3f81e29d09281702f5057a1f.js"
 );
 
 workbox.clientsClaim();
