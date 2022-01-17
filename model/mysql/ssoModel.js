7953174bdb083dd66da661d10762816110922370
dexitional <ebenezerkb.ackah@gmail.com>
2022-01-17 14:23:39 +0000

    admi fiixed

--- model/mysql/ssoModel.js
+++ model/mysql/ssoModel.js
@@ -490,8 +490,12 @@ module.exports.SSO = {
          
          if(bid){
             // Insert Academic Fees or Bill charged
-            const df = { session_id:vs[0].academic_session_id, bill_id:bid, refno:data.serial, narrative: bl[0].narrative, currency:bl[0].currency,currency:bl[0].amount }
+            const df = { session_id:vs[0].academic_session_id, bill_id:bid, refno:data.serial, narrative: bl[0].narrative, currency:bl[0].currency, amount:bl[0].amount }
             await db.query("insert into fms.studtrans set ?", df)
+            // Insert Discount (Payment)
+            const dj = { session_id:vs[0].academic_session_id, bill_id:bid, refno:data.serial, narrative: `DISCOUNT ON ${bl[0].narrative} FEES`, currency:bl[0].currency, amount:(-1 * bl[0].discount)}
+            await db.query("insert into fms.studtrans set ?", df)
+            
          }
          return { ...da,...dp,...dm,...du, program:pg[0].short, phone:sp[0].phone }
 
